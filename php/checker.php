f448903cae13c17fe19e750f560ef704df8e85ed
Rafalstrus <rafal.strus@onet.eu>
2023-11-03 21:22:45 +0100

    small rewrite

--- php/checker.php
+++ php/checker.php
@@ -5,7 +5,8 @@ namespace Checker;
 class Checker
 {
     const NUMBER_OF_REPETITIONS = 100;
-    const JIT_ENABLED = true;
+
+    private static ?bool $isJitEnabled = null;
 
     public static function check(string $path): void
     {
@@ -55,18 +56,24 @@ class Checker
 
     private static function saveResult($path, $script, $time, $memory): void
     {
-        if (self::JIT_ENABLED) {
-            file_put_contents(__DIR__ . '/jit_result/' . $path . '/' . $script . '.txt', 't: ' . $time . ', m: ' . $memory . PHP_EOL, FILE_APPEND);
-        } else {
-            file_put_contents(__DIR__ . '/result/' . $path . '/' . $script . '.txt', 't: ' . $time . ', m: ' . $memory . PHP_EOL, FILE_APPEND);
+        if (is_null(self::$isJitEnabled)) {
+            self::$isJitEnabled = isset(opcache_get_status()["jit"]["enabled"]) && opcache_get_status()["jit"]["enabled"];
         }
+
+        if (self::$isJitEnabled) {
+            $textToAppend = 'jit. ';
+        }
+
+        $textToAppend .=  't: ' . $time . ', m: ' . $memory . PHP_EOL;
+
+        file_put_contents(__DIR__ . '/result/' . $path . '/' . $script . '.txt', $textToAppend, FILE_APPEND);
     }
 }
 
-Checker::check('arrayPush');
-Checker::check('instance');
-Checker::check('loop');
-Checker::check('mapArray');
-Checker::check('null');
-Checker::check('object');
-Checker::check('statement');
+// Checker::check('arrayPush');
+// Checker::check('instance');
+// Checker::check('loop');
+// Checker::check('mapArray');
+// Checker::check('null');
+// Checker::check('object');
+// Checker::check('statement');
